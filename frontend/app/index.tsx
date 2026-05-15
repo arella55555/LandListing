5cb6f178faed353a85fa84df79a054640253aa45
nevergiveup222 <blo0mle4f@gmail.com>
2026-05-16 05:29:36 +0800

    Changed index to use appnav.

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,5 +1,13 @@
 import HomeScreen from "../src/screens/Home/HomeScreen";
+import Dashboard from "../src/screens/Dashboard/Dashboard";
+import React from "react";
+import AppNavigator from "../src/navigation/AppNavigation";
+//import { AuthProvider } from "../src/navigation/AuthContext";
 
 export default function App() {
-  return <HomeScreen />;
+    return (
+        //<AuthProvider>
+            <AppNavigator />
+        //</AuthProvider>
+    );
 }
---
+++ frontend/src/navigation/AppNavigation.tsx
@@ -0,0 +1,32 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+
+import Home from '../screens/Home/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MainTabs() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={Home}/>
+        </Tab.Navigator>
+    );
+}
+
+export default function AppNavigator() {
+    return (
+        //<NavigationContainer>
+            <Stack.Navigator>
+               
+                <Stack.Screen 
+                    name="Main"
+                    component={MainTabs}
+                    options={{headerShown: false }}
+                />
+            </Stack.Navigator>
+        //</NavigationContainer>
+    );
+}
