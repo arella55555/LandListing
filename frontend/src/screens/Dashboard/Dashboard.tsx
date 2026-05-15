2df19d3fad74031d897c202c894f193a4762ad81
nevergiveup222 <blo0mle4f@gmail.com>
2026-05-16 05:41:08 +0800

    Deleted

--- frontend/src/screens/Dashboard/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-// Accessing the property 'toLowerCase'
-// on 'message' and then calling it
-const message = "Hello World!";
-//As you can probably guess, if we try to run
-message.toLowerCase();
-// Calling 'message'
-//message();
-
